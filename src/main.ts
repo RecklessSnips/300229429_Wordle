39bb6dee48b4a1704dc62e01b4c2796b2b8df1d1
unknown <rliu144@uottwa.ca>
2024-06-04 17:06:54 -0400

    Finish basic layout and check board logic, utilize the datamuse.com API for English word checking.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
 // import './assets/main.css'
 
+//in main.js
+import 'primevue/resources/themes/aura-light-green/theme.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
 import App from './App.vue'
+import Message from 'primevue/message';
+import Button from 'primevue/button';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(PrimeVue);
+app.component('Message', Message);
+app.component('Button', Button);
+app.component('Toast', Toast);
+app.use(ToastService);
 
 app.mount('#app')
